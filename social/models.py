05d227ae4076f3fe8dbcd464c3c0b5a49ab90d41
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-11-23 16:05:09 +0000

    added forms.py

--- social/models.py
+++ social/models.py
@@ -16,3 +16,6 @@ class Post(models.Model):
     content = models.TextField()
     image = models.ImageField(upload_to='posts/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'Post by {self.user.username} at {self.created_at}'
