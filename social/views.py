916b56a861e1e1a9156d5ca3353491e91b281c22
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-11-23 15:59:57 +0000

    added create form in views.py social

--- social/views.py
+++ social/views.py
@@ -6,3 +6,16 @@ def feed(request):
     posts = Post.objects.all().order_by('-created_at')
     return render(request, 'social/feed.html', {'posts': posts})
 
+@login_required
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST, request.FILES)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+            return redirect('feed')
+    else:
+        form = PostForm()
+
+    return render(request, 'social/create_post.html', {'form': form})
