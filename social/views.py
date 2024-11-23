f1c5564fd74aa913c60c90ec8a50dee235f6528e
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-11-23 16:39:03 +0000

    added render registration form and def the submission

--- social/views.py
+++ social/views.py
@@ -33,3 +33,14 @@ def booking(request):
     
     return render(request, 'booking.html')
 
+def register(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user=form.save()
+            login(request, user)
+            return redirect('home')
+    else:
+        form = CustomUserCreationForm()
+
+    return render(request, 'registration/register.html', {'form': form})
