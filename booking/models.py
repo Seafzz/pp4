ac6f0134ecc120bebf26df4dc70290cc70974315
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-11-23 15:32:14 +0000

    added bookingmodel to admin

--- booking/models.py
+++ booking/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-class Booking(models.model):
-    name = model.CharField(max_length=100)
+class Booking(models.Model):
+    name = models.CharField(max_length=100)
     email = models.EmailField()
-    date = models.DateTimeFIeld()
+    date = models.DateTimeField()
 
     def __str__(self):
         return self.name
