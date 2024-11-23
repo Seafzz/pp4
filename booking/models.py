dfc06c072697c5d976b798500bac4e62687f303c
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-11-23 15:30:57 +0000

    fix: typo in models

--- booking/models.py
+++ booking/models.py
@@ -1,9 +1,9 @@
 from django.db import models
 
-class Booking(models.moidel):
+class Booking(models.model):
     name = model.CharField(max_length=100)
     email = models.EmailField()
     date = models.DateTimeFIeld()
 
-    def __str__(self)
+    def __str__(self):
         return self.name
