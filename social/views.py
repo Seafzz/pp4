fc1f86008ac12c86ea7c89cbe6ad61a157ae34f6
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-11-23 15:57:59 +0000

    added feed to views in social

--- social/views.py
+++ social/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from .models import Post
 
-
+def feed(request):
+    posts = Post.objects.all().order_by('-created_at')
+    return render(request, 'social/feed.html', {'posts': posts})
 
