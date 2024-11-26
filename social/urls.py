99f603c6a200257b186f7c29594acdc0e64821de
Alexander Andersson <Alexander.wirdbo@hotmail.se>
2024-11-26 19:18:13 +0000

    Update: updated the Urls and adding the missing ones

--- social/urls.py
+++ social/urls.py
@@ -2,6 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('feed/', views.feed, name='feed'),
     path('create-post/', views.create_post, name='create_post'),
+    path('booking/', views.booking, name='booking'),
+    path('register/', views.register, name='register'),
 ]
